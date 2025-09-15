da171a16590c8f0039b0aa7f0918c1a431b11bdc
kairi717 <wooya1745@gmail.com>
2025-09-15 13:06:47 +0900

    Fix : Firebase 적용한 Oauth방식

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 // api/index.js
 const userHandler = require('./user.js');
 const rankingHandler = require('./ranking.js');
+const testHandler = require('./test.js').default; // .default를 추가하여 ES 모듈을 가져옵니다.
 
 module.exports = async (req, res) => {
   const { url } = req;
@@ -11,6 +12,10 @@ module.exports = async (req, res) => {
 
   if (url.startsWith('/api/ranking/')) {
     return rankingHandler(req, res);
+  }
+
+  if (url.startsWith('/api/test')) {
+    return testHandler(req, res);
   }
 
   res.status(404).json({ message: 'Global API endpoint not found.' });
